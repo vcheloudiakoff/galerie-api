0f7f62c787949ec431ecdc3df30592acf82859ae
Charlon <charles.assus@gmail.com>
2019-10-03 14:06:40 +0200

    Add mandatory fields

--- app/graphql/artists/type.rb
+++ app/graphql/artists/type.rb
@@ -1,20 +1,20 @@
 Artists::Type = GraphQL::ObjectType.define do
   name 'Artist'
   field :id, !types.String
-  field :exhibition_ids, types[types.String] do
+  field :exhibition_ids, !types[!types.String] do
     resolve CollectionIdsResolver
   end
-  field :exhibitions, types[Exhibitions::Type]
-  field :artwork_ids, types[types.String] do
+  field :exhibitions, !types[!Exhibitions::Type]
+  field :artwork_ids, !types[!types.String] do
     resolve CollectionIdsResolver
   end
-  field :artworks, types[Artworks::Type]
-  field :created_at, types.String
-  field :updated_at, types.String
-  field :first_name, types.String
-  field :last_name, types.String
+  field :artworks, !types[!Artworks::Type]
+  field :created_at, !types.String
+  field :updated_at, !types.String
+  field :first_name, !types.String
+  field :last_name, !types.String
   field :nickname, !types.String
-  field :description, types.String
+  field :description, !types.String
   field :preview_artwork_id, types.String
   field :preview_artwork, Artworks::Type
 
